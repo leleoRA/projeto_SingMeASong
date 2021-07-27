7be720a75ec27fa1b7d1cbc65379bd4866840fd3
leleo <celso@respondeai.com.br>
2021-07-27 16:51:27 -0300

    preparando review

--- tests/utils/database.ts
+++
@@ -1,9 +0,0 @@
-import connection from "../../src/database";
-
-export async function clearDatabase() {
-  await connection.query(`TRUNCATE items RESTART IDENTITY`);
-}
-
-export async function closeConnection() {
-  await connection.end();
-}
